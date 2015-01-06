df961487db9d6cbbbe0418155ff92f6f48d707e0
Max Zheng <mzheng@linkedin.com>
2015-01-06 00:17:55 -0800

    Updated to use latest localconfig: Refactor to use LocaConfig and change where cache_duration can be set

--- src/remoteconfig/manager.py
+++ src/remoteconfig/manager.py
@@ -1,36 +1,31 @@
-from localconfig.manager import DotNotationConfig
+from localconfig.manager import LocalConfig
 import requests
 
 from remoteconfig.utils import url_content
 
 
-class RemoteConfig(DotNotationConfig):
-  def __init__(self, last_source=None, default_cache_duration=None, **localconfig_kwargs):
+class RemoteConfig(LocalConfig):
+  def __init__(self, last_source=None, cache_duration=None, **localconfig_kwargs):
     """
     :param file/str last_source: Last config source, file or URL. This source is only read when an attempt to read a
                                  config value is made (delayed reading, hence "last") if it exists.
-    :param int default_cache_duration: For URL source only. Optionally cache the URL content for the given duration (seconds) to
+    :param int cache_duration: For URL source only. Optionally cache the URL content for the given duration (seconds) to
                                avoid downloading too often. This sets the default for all subsequent :meth:`self.read` calls.
-    :param dict localconfig_kwargs: Additional keyword args to be passed to localconfig's :meth:`DotNotationConfig.__init__`
+    :param dict localconfig_kwargs: Additional keyword args to be passed to localconfig's :meth:`LocalConfig.__init__`
     """
 
     #: Default cache duration when reading from a URL source
-    self._cache_duration = default_cache_duration
+    self._cache_duration = cache_duration
 
     super(RemoteConfig, self).__init__(last_source, **localconfig_kwargs)
 
-  def read(self, source, cache_duration=None):
+  def _read(self, source):
     """
     Reads and parses the config source
 
     :param file/str source: Config source URL (http/https), or string, file name, or file pointer.
-    :param int cache_duration: For URL source only. Optionally cache the URL content for the given duration (seconds) to
-                               avoid downloading too often. Overrides the default if that was set during instantiation.
     """
-    if not cache_duration:
-      cache_duration = self._cache_duration
-
     if source.startswith('http://') or source.startswith('https://'):
-      source = url_content(source, cache_duration, from_cache_on_error=True)
+      source = url_content(source, cache_duration=self._cache_duration, from_cache_on_error=True)
 
-    return super(RemoteConfig, self).read(source)
+    return super(RemoteConfig, self)._read(source)
--- test/test_remoteconfig.py
+++ test/test_remoteconfig.py
@@ -1,3 +1,5 @@
+import time
+
 import httpretty
 
 from remoteconfig import config, RemoteConfig
@@ -13,20 +15,17 @@ class TestRemoteConfig(object):
     config.read(config_url)
     assert config_content == str(config)
 
-    # Cache duration on read
+    # Using cache duration
     updated_config_content = '[section]\n\nkey = updated\n'
     httpretty.register_uri(httpretty.GET, config_url, body=updated_config_content)
-    config.read(config_url, cache_duration=10)
-    assert config_content == str(config)
-
-    # Default cache duration
-    config2 = RemoteConfig(config_url, default_cache_duration=10, kv_sep=': ')
+    config2 = RemoteConfig(config_url, cache_duration=1, kv_sep=': ')
     assert config_content.replace(' = ', ': ') == str(config2)
 
-    # Using default cache duration
+    # Still under cache duration
     config2.read(config_url)
     assert config_content.replace(' = ', ': ') == str(config2)
 
-    # Should update without cache duration
+    time.sleep(1)
+    # Should update after cache duration
     config.read(config_url)
     assert updated_config_content == str(config)
